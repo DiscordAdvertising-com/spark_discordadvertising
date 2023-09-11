3969e9b8dedf3f8114e1defaa45816686f602add
Bacio001 <bacio0011@gmail.com>
2023-09-11 22:48:41 +0200

    Email

--- app/Http/Livewire/Pages/Public/contact.php
+++ app/Http/Livewire/Pages/Public/contact.php
@@ -6,26 +6,41 @@ use App\Models\Bot;
 use App\Models\Tag;
 use App\Models\BotTag;
 use App\Models\Server;
+use GuzzleHttp\Client;
 use Livewire\Component;
 use Illuminate\Support\Facades\DB;
 
-class contact extends Component
+class Contact extends Component
 {
+
+    public $email;
+    public $name;
+    public $description;
+    public $sent = false;
 
     public function render()
     {        
-
-        $latestBots = [];
-        $latestServers = [];
-
-
-        $latestBots = Bot::where('status', 'Accepted')->orderBy('created_at', 'DESC')->limit(4)->get();
-
-        $latestServers = Server::where('status', 'Accepted')->orderBy('created_at', 'DESC')->limit(4)->get();
-
-        return view('livewire.pages.public.contact', ['latestBots' => $latestBots, 'latestServers' => $latestServers])
+        return view('livewire.pages.public.contact')
         ->extends('layouts.app')
         ->section('content');
+    }
+
+    public function contact() {
+
+        $client = new Client();
+
+        $embed = (object)array();
+        $embed->title = $this->name .' | '. $this->email  ?? 'No Info';
+        $embed->description = $this->description ?? 'No Description';
+        $embed->color = hexdec('#00F700');
+
+        $client->post('https://discord.com/api/v9/channels/1123608372659359805/messages', ['headers' => ['Authorization' => 'Bot '.config('services.discord.bot_token_webhooks'), 'Content-Type'=> 'application/json'], 'json' => ['embeds' => [$embed]]]);
+
+        $this->name = null;
+        $this->email = null;
+        $this->description = null;
+        $this->sent = true;
+
     }
 
 }
--- resources/views/livewire/pages/public/contact.blade.php
+++ resources/views/livewire/pages/public/contact.blade.php
@@ -1,81 +1,34 @@
+<div>
+  @include('layouts.header')
 
-<div> 
+  <div class="w-full flex flex-col text-gray-400 gap-5 mt-48">
 
-    <div class="w-full mt-32">
-        <div class="w-full relative">
-            <div class="font-bold tracking-widest">
-                <h1 class="text-accent">Discord Advertising</h1>
-                <h1 class="text-white text-3xl">Contact Form</h1>
-                <br>
-            </div>
-        </div>
+    <div class="w-full relative">
+      <div class="font-bold tracking-widest">
+          <h1 class="text-accent">Contact</h1>
+          <h1 class="text-white text-3xl">Contact us trough the form below</h1>
+          <br>
+      </div>
+  </div>
 
+    <div class="w-full flex flex-col gap-2">
+      <label for="name">Name</label>
+      <input wire:model="name" class="w-full bg-input p-4 rounded-lg focus:ring-2 focus:ring-accent  focus:outline-none text-gray-200" type="text">
     </div>
-    <!-- contact section -->
-    <section class="section" id="contact">
-        <div class="container text-center"> 
-            <!-- contact form -->
-            <form id="form" class="contact-form col-md-10 col-lg-8 m-auto">
-                <div class="form-row">
-                    <div class="font-bold tracking-widest">
-                        <input type="text" size="50" class="text-white text-1xl"name="name" id="name"  placeholder="Your Name" required>                
-                    </div>
-                    <div class="font-bold tracking-widest">
-                        <input type="email" size="50" class="text-white text-1xl" name="email" id="email" placeholder="Enter Email"requried>              
-                    </div>
-                    <div class="font-bold tracking-widest">
-                        <textarea name="comment" id="comment" rows="1"
-                        class="text-white text-1xl" size="50" placeholder="Write Something"></textarea>
-                    </div>
-                    <div class="form-group col-sm-12 mt-3">
-                      <input type="submit" value="Send Message" id="button" class="btn btn-outline-primary rounded">
-                  </div>
-              </form>
+    
+    <div class="w-full flex flex-col gap-2">
+      <label for="email">Email</label>
+      <input wire:model="email" class="w-full bg-input p-4 rounded-lg focus:ring-2 focus:ring-accent  focus:outline-none text-gray-200" type="email">
+    </div>
+
+    <div class="w-full flex flex-col gap-2">
+      <label for="description">Description</label>
+      <textarea wire:model="description" class="w-full bg-input p-4 rounded-lg focus:ring-2 focus:ring-accent  focus:outline-none text-gray-200" rows="7"></textarea>
+    </div>
+
+    <button wire:click="contact" class="w-fit text-lg @if($sent) bg-green-500 @else bg-accent @endif rounded-lg text-white font-semibold p-3 px-5"> <li class="@if($sent) fa-solid fa-check @else fa-solid fa-paper-plane @endif mr-1"></li> @if($sent) Sent @else Send @endif</button>
+
+
+  </div>
 
 </div>
-
-        
-            <script type="text/javascript"
-              src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
-            
-            <script type="text/javascript">
-              emailjs.init('qKFOvpUbKKhtE4BRT')
-            </script>
-
-<script>
-    const btn = document.getElementById('button');
-
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
-
-   btn.value = 'Sending...';
-
-   const serviceID = 'default_service';
-   const templateID = 'template_g7cfb67';
-
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
-});
-  </script>
-
-<script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/64f936f1b2d3e13950ee65f6/1h9mnt8g4';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-    })();
-    </script>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3693059889859602"
-    crossorigin="anonymous">
-</script>
